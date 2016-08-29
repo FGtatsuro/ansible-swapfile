36dc2d7d574b5375a7991d01c4977353327233ea
FGtatsuro <fujiistorage@gmail.com>
2016-08-29 14:25:10 +0900

    Add specs for swapfile.

--- spec/swapfile_spec.rb
+++ spec/swapfile_spec.rb
@@ -1,2 +1,12 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
+describe file('/swapfile') do
+  it { should be_file }
+  it { should be_mode 600 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe command('swapon -s') do
+  its(:stdout) { should contain('/swapfile') }
+end
